cd7ef875566f30ef5c274cdc33a6733f75afd6be
ngoldack <nicolas-goldack@live.de>
2023-09-09 17:47:05 +0200

    removed svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import markdoc from "@astrojs/markdoc";
 
-import svelte from "@astrojs/svelte";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({
     analytics: true,
   }),
-  integrations: [tailwind(), markdoc(), svelte()],
+  integrations: [tailwind(), markdoc()],
 });
